8136ae44e6c3ab007578ee35d05e22b20864d218
Muhammad Taha <muhammad9675581@gmail.com>
2025-03-26 13:35:13 +0500

    first commit

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
+
+const inter = Inter({
+  variable: "--font-inder",
+  subsets: ["latin"],
+});
+
+
+export const metadata = {
+  title: "Deepseek - A Next.js Starter",
+  description: "Full Stack Project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <AppContextProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+    </AppContextProvider>
+    </ClerkProvider>
+  );
+}
