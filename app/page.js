9e9bc5262ac6592c9bab2d9656b79781aad7766c
Muhammad Taha <muhammad9675581@gmail.com>
2025-03-26 16:22:47 +0500

    changes done

--- app/page.js
+++ app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
           <>
           <div className="flex items-center gap-3">
             <Image src={assets.logo_icon} alt="" className="h-16"/>
-            <p className="text-2xl font-medium">Hi,I'm DeepSeek.</p>
+            <p className="text-2xl font-medium">Hi, I&apos;m DeepSeek.</p>
           </div>
           <p className="text-sm mt-2">How can I help you today?</p>
           </>
